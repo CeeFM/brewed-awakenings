fadc7aa823a2150fb5b3888d829224df5503d78e
Colin Matteson <seefm@proton.me>
2023-09-19 15:04:48 -0400

    added event listener to show product prices upon click event

--- scripts/Products.js
+++ scripts/Products.js
@@ -14,3 +14,14 @@ export const Products = () => {
     return html
 }
 
+document.addEventListener('click', e => {
+    const itemClicked = e.target;
+    if (itemClicked.id.startsWith("product")) {
+        const [,productPrimaryKey] = itemClicked.id.split("--");
+        for (const product of products) {
+            if (product.id === parseInt(productPrimaryKey)) {
+                        window.alert(`${product.name} costs $${product.price.toFixed(2)}. Don't even think about stealing it though. We're watching you. I would insert emoji eyes here if I could. I'm not a friggin wizard tho.`)
+                    }
+                }
+            }
+        })
